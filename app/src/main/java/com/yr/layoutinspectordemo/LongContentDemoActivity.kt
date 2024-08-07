97106df57ca37e984251aa0f24736ca0fdc4c787
yrwu <dreambo4@gmail.com>
2024-08-08 00:22:20 +0800

    新增UI Demo、新增滑動Demo

--- app/src/main/java/com/yr/layoutinspectordemo/LongContentDemoActivity.kt
+++ app/src/main/java/com/yr/layoutinspectordemo/LongContentDemoActivity.kt
@@ -1,34 +1,20 @@
 package com.yr.layoutinspectordemo
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LongContentDemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_long_content_demo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        findViewById<Button>(R.id.gotoBaseline).setOnClickListener {
-            startActivity(Intent(this, BaselineDemoActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.gotoUI).setOnClickListener {
-            startActivity(Intent(this, UIDemoActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.gotoLongContent).setOnClickListener {
-            startActivity(Intent(this, LongContentDemoActivity::class.java))
         }
     }
 }
